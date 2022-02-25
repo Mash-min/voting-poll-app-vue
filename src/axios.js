3bc19d5440b46cfdbd33ea345cc900831e76e13d
Mashiyyat <delossantos.mashiyyat@gmail.com>
2022-02-25 09:36:55 +0800

    Axios url changed

--- src/axios.js
+++ src/axios.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000/'
+// axios.defaults.baseURL = 'http://127.0.0.1:8000/'
+axios.defaults.baseURL = 'https://mm-voting-poll-app.herokuapp.com/'
 axios.defaults.headers['Authorization'] = 'Bearer ' + localStorage.getItem('token')
